17e31403700c87e96f12374e8ca8b7c973bc8b44
junior22ferreira <junioredilsonferreira@hotmail.com>
2022-07-22 20:09:24 -0300

    Teste

---
+++ Teste.py
@@ -0,0 +1,23 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+#Layout
+sg.theme('Reddit')
+layout = [
+    [sg.Text('User'), sg.Input(key='usuario')]
+    [sg.Text('password'), sg.Input(key='senha',password_char='*')]
+    [sg.Checkbox('Save the password')]
+    [sg.Button('Login')]
+]
+
+#Janela
+janela =sg.Window('Login screen', layout)
+
+#Ler os eventos
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['usuario'] == '' and valores['senha'] == '':
+            print ('Bem Vindo')
+            
