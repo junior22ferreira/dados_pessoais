299036b3cb9c1f3d7156fdadfdf4628c0d5fdee2
junior22ferreira <junioredilsonferreira@hotmail.com>
2022-07-23 02:07:58 -0300

    Teste

--- Teste.py
+++ Teste.py
@@ -3,21 +3,23 @@ import PySimpleGUI as sg
 #Layout
 sg.theme('Reddit')
 layout = [
-    [sg.Text('User')], [sg.Input(key='usuario')]
-    [sg.Text('password')], [sg.Input(key='senha',password_char='*')]
-    [sg.Checkbox('Save the password')]
-    [sg.Button('Login')]
+    [sg.Text('User')], 
+    [sg.Input(key='usuario')],
+    [sg.Text('password')], 
+    [sg.Input(key='senha',password_char='*')],
+    [sg.Checkbox('Save the password')],
+    [sg.Button('Login'), sg.Button('Cancel')],
 ]
 
 #Janela
-janela =sg.Window('Login screen', layout=layout)
+janela = sg.Window('Login screen', layout)
 
 #Ler os eventos
 while True:
-    eventos, valores = janela.read()
-    if eventos == sg.WINDOW_CLOSED:
+    eventos, valores = janela.read(),
+    if eventos == sg.WINDOW_CLOSED or Event == 'Cancel':
         break
     if eventos == 'Entrar':
         if valores['usuario'] == '' and valores['senha'] == '':
-            print ('Bem Vindo')
+         print ('Bem Vindo!')
             
